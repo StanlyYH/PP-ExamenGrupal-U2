62f62b082136835f83ac36991271fa4a0dd48ee6
darlanmejia <daelanjacob@gmail.com>
2026-07-11 03:13:08 -0600

    Agregadas las migracion, Scalar y pruebas Bruno

--- Program.cs
+++ Program.cs
@@ -1,16 +1,28 @@
 using ApiCuentaAhorros.Extensiones;
+using ApiCuentaAhorros.Servicios.Calculos;
+using ApiCuentaAhorros.Servicios.Simulaciones;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AgregarServiciosAplicacion(
-    builder.Configuration
-);
+builder.Services.AgregarServiciosAplicacion(builder.Configuration);
+
+
+builder.Services.AddTransient<IServicioCalculos, ServicioCalculos>();
+
+builder.Services.AddTransient<IServicioSimulaciones, ServicioSimulaciones>();
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
+
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
